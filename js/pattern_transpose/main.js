13f3723601fbbaedf30e5c2b4156b28f913dabc0
Jakob Penca <jpenca@me.com>
2017-02-16 00:19:10 +0100

    updated style

--- js/pattern_transpose/main.js
+++ js/pattern_transpose/main.js
@@ -3,22 +3,32 @@ window.onload = () => {
 
 	console.clear()
 	console.log('doc loaded')
+
+	var trasnposeContainer = document.createElement('section')
+		trasnposeContainer.classList.add('transpose')
+	
+	document.body.appendChild(trasnposeContainer)
+	let title = document.createElement('h1')
+	title.innerHTML = 'Transpose Current Pattern'
+	trasnposeContainer.appendChild(title)
+
+
 	for(var i = 12; i >= -12; i--) {
 		
 		if(i == 0)
 			continue
 
-		addBtn(i);
+		addBtn(trasnposeContainer, i);
 	}
 }
 
-function addBtn(note) {
+function addBtn(parent, note) {
 	console.log('adding button for note: ' + note);
 	var btn = document.createElement('button');
 	btn.setAttribute('content', note);
 	btn.className = 'button';
 	btn.innerHTML = note;
-	document.body.appendChild(btn); 
+	parent.appendChild(btn); 
 
 	btn.onclick = () => {
 		transpose(btn, note);
@@ -44,29 +54,17 @@ function transpose(btn, note) {
 
 	console.log('transposing ' + note)
 	s.getPattern().then(pattern => {
-
-		console.log(pattern)
-
-
 		pattern.tracks.forEach((track, tIndex) => {
-			
-			// console.log('trk ' + index)
 			track.settings.trig.note = foldNoteValue(track.settings.trig.note + note)
-			
 			track.steps.forEach((step, sIndex) => {
 				if(step.note != null) {
-					
 					step.note = foldNoteValue(step.note + note)
 				}
 			})
 		})
-		
-		
-		console.log(pattern)
 
 		s.post(pattern).then(result => {
 			console.log('done!')
 		})
-		
 	})
 }
